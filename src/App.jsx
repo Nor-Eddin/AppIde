ef9e390dd76cbeb99d922fb70a89a5cdb97388ed
Nor-Eddin <noreddinlam@gmail.com>
2024-03-31 18:41:58 +0200

    Implementation de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-900 min-h-screen flex flex-col">
+      <div className="px-4 py-4 boder-b flex items-center">
+        <h1 className="w-full text-slate-50 text-2xl">
+          <span className="text-base">The</span>SmartIDE
+        </h1>
+        <div className="w-full">
+          <button className="text-white bg-blue-800 p-2 rounded">Show preview</button>
+        </div>
+        <div className="w-full flex">
+          <div className="ml-auto mr-2">
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full"></div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
