7e617b16eaf1ac0e098d2a66fd593b31e98940c1
Nor-Eddin <noreddinlam@gmail.com>
2024-03-31 19:08:07 +0200

    code de la sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Tabs from "./components/Tabs";
+
+
 function App() {
   return (
     <div className="bg-zinc-900 min-h-screen flex flex-col">
-      <div className="px-4 py-4 boder-b flex items-center">
+      <div className="px-4 py-4 boder-b flex items-center border-b border-slate-200">
         <h1 className="w-full text-slate-50 text-2xl">
           <span className="text-base">The</span>SmartIDE
         </h1>
@@ -13,10 +16,11 @@ function App() {
             <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
             <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
             <div className="w-[6px] h-[6px] bg-slate-200 rounded-full"></div>
-            
+
           </div>
         </div>
       </div>
+      <Tabs/>
     </div>
   )
 }
---
+++ src/components/CodeTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CodeTab() {
+  return (
+    <div>
+      <textarea  spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
+
+      </textarea>
+    </div>
+  )
+}
