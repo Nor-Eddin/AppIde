a669e02624c7df66fd67b2674007480ae295d91f
Nor-Eddin <noreddinlam@gmail.com>
2024-04-01 12:02:09 +0200

    Mise en place de redux

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function CodeTab() {
+
+  const tabsState= useSelector(state => state.tabs);
+console.log(tabsState);
   return (
     <div>
       <textarea  spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
