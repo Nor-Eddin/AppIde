61a7c0f350a385f313d74282934486e7ba430ac4
Nor-Eddin <noreddinlam@gmail.com>
2024-04-02 18:36:28 +0200

    Ajout de la feature de changement de code

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { updateCode } from '../features/tabs';
 
 
 export default function CodeTab({code,id}) {
-
+const dispath = useDispatch();
 
   return (
     <div>
-      <textarea value={code} spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
+      <textarea onChange={e=>dispath(updateCode({id,value:e.target}))} value={code} spellCheck="false" 
+      className='bg-zinc-900 text-slate-200 text-xl 
+      p-8 block h-full w-full focus:outline-none resize-none'>
 
       </textarea>
     </div>
